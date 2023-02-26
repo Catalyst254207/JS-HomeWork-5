7cc7d165af35a80985124491e09f2064ed3eb0c9
Roman <roman_federov@mail.ru>
2023-02-26 12:10:38 +0700

    homework#5 vol 2

--- script.js
+++ script.js
@@ -52,20 +52,55 @@
 // Задание 3
 // Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
 
-const products = [
-{
-id: 3,
-price: 200,
-},
-{
-id: 4,
-price: 900,
-},
-{
-id: 1,
-price: 1000,
-},
-];
+// const products = [
+// {
+// id: 3,
+// price: 200,
+// },
+// {
+// id: 4,
+// price: 900,
+// },
+// {
+// id: 1,
+// price: 1000,
+// },
+// ];
 
-products.forEach(prod => prod.price = prod.price * 0.85);
-console.log(products);
+// products.forEach(prod => prod.price = prod.price * 0.85);
+// console.log(products);
+
+// Задание 4
+// 1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод filter. Исходные данные - массив products.
+// 2. Необходимо отсортировать массив products используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+
+// const products = [
+//     {
+//     id: 3,
+//     price: 127,
+//     photos: [
+//             "1.jpg",
+//             "2.jpg",
+//             ],
+//     },
+//     {
+//     id: 5,
+//     price: 499,
+//     photos: [],
+//     },
+//     {
+//     id: 10,
+//     price: 26,
+//     photos: [
+//             "3.jpg",
+//             ],
+//     },
+//     {
+//     id: 8,
+//     price: 78,
+//     },
+//     ];
+
+
+//     let thereIsPhoto = products.filter(products => products.photos !== 0);
+//     console.log(thereIsPhoto);
